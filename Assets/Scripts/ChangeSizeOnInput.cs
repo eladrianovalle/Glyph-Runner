4f80a279693a513b54072415c22a2bc6cbc26be8
Adriano Valle <adrianovalle@me.com>
2016-10-21 13:46:09 -0400

    Refactored code base.  Created PlayerCharacter object to hold player data.  Refactored jump script.

---
+++ Assets/Scripts/ChangeSizeOnInput.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeSizeOnInput : MonoBehaviour {
+
+	public PlayerCharacter player;
+	
+	void Update () {
+
+		// increase character size
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			
+			if (player.charSize < 3) { 
+				
+				print ("I'm size" + player.charSize + "and pressed up!");
+				player.charSize += 1;
+				AudioSource.PlayClipAtPoint(player.grow, this.transform.position, player.sfxVolume);
+
+			}
+
+			player.SizeChange();
+
+		}
+
+		// decrease character size
+		if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			
+			if (player.charSize > 1) { 
+				
+				print ("I'm size" + player.charSize + "and pressed down!");
+				player.charSize -= 1;
+				AudioSource.PlayClipAtPoint(player.shrink, this.transform.position, player.sfxVolume);
+
+			}	
+
+			player.SizeChange();
+
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCharacter : MonoBehaviour {
+
+	// player component variables
+	public SpriteRenderer _sRenderer;
+	public BoxCollider2D _collider;
+	public Rigidbody2D _rBody;
+
+	// player state variables
+	public enum PlayerState { Cat, Man, God };
+	public PlayerState currentState;
+	public int charSize;
+
+	// audio variables
+	public float sfxVolume;
+	public AudioClip grow;
+	public AudioClip shrink;
+
+	// player gameobject variables
+	public GameObject[] playerObjStates;
+
+
+	void Start () {
+
+		// get components
+		_sRenderer = this.GetComponent<SpriteRenderer> ();
+		_collider = this.GetComponent<BoxCollider2D> ();
+		_rBody = this.GetComponent<Rigidbody2D> ();
+
+
+		// set character size and state
+		charSize = 1;
+		SizeChange ();
+	}
+
+
+	public void SizeChange () {
+
+		if (charSize == 1) {
+
+			currentState = PlayerCharacter.PlayerState.Cat;
+
+			playerObjStates[1].SetActive(false);
+			playerObjStates[2].SetActive(false);
+
+			playerObjStates[0].SetActive(true);
+
+		}
+
+		if (charSize == 2) {
+
+			currentState = PlayerCharacter.PlayerState.Man;
+
+			playerObjStates[0].SetActive(false);
+			playerObjStates[2].SetActive(false);
+
+			playerObjStates[1].SetActive(true);
+
+		}
+
+		if (charSize == 3) {
+
+			currentState = PlayerCharacter.PlayerState.God;
+
+			playerObjStates[0].SetActive(false);
+			playerObjStates[1].SetActive(false);
+
+			playerObjStates[2].SetActive(true);
+
+		}
+	}
+
+
+}
